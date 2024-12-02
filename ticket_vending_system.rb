9b94fbb00903824d85fe5180d1fd7e07b36eb6f3
kimu805 <kimukimuok805@gmail.com>
2024-12-02 14:46:40 +0900

    Update ticket_vending_system.rb

--- ticket_vending_system.rb
+++ ticket_vending_system.rb
@@ -1,3 +1,16 @@
+require "date"
+
 class TicketVendingSystem
-  
+
+  attr_reader :rides, :created_at
+
+  def initialize
+    @ride_info = [
+      { name: "roller coaster", fee: 1200 },
+      { name: "merry-go-round", fee: 1000 },
+      { name: "jackie coaster", fee: 800 }
+    ]
+    @created_at = Date.today
+  end
 end
+
