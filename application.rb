186527e64c8cb2744874867947c3d2cabbda27c1
kimu805 <kimukimuok805@gmail.com>
2024-12-02 15:23:25 +0900

    Update application.rb

--- application.rb
+++ application.rb
@@ -1,3 +1,11 @@
 require "./ticket_vending_system"
+require "./ride"
 
-TicketVendingSystem.new.exec_transaction
+rides = [
+  { name: "roller coaster", fee: 1200 },
+  { name: "merry-go-round", fee: 1000 },
+  { name: "jackie coaster", fee: 800 }
+]
+
+rides_info = rides.map { |b| Ride.new(b[:name], b[:fee]) }
+TicketVendingSystem.new(rides_info).exec_transaction
